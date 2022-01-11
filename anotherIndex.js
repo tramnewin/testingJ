04678f2c6ddf28a40259bee35077b018a60ab669
tramnewin <trmnguyen2608@gmail.com>
2022-01-10 22:53:07 -0700

    third commit

--- anotherIndex.js
+++ anotherIndex.js
@@ -14,6 +14,8 @@ var myGlobal = 10;
 function func1(){
     oopsGlobal = 5; //no var keyword, within this scope, global
     //BUT IF IT HAS VAR AT THE FRONT, THE IT WOULD BE DECLARED AS A LOCAL VARIABLE
+    var thisISlocal = 5;
+
 }
 
 function func2(){
@@ -25,7 +27,43 @@ function func2(){
         output += " oopsGlobal: " + oopsGlobal;
 
     }
+    if (typeof thisISlocal != "undefined"){
+        output += " oopsGlobal: " + thisISlocal;
+
+    }
     console.log(output);
 }
 func1();
-func2()
+func2();
+
+function myLocalScope(){
+    var myVar = 5;
+    console.log(myVar);
+}
+myLocalScope();
+//console.log(myVar); ERROR: The var is not local
+
+
+var outerWear = "Tees";
+function myOutfit(){
+    var outerWear  ="sweater";
+    return outerWear;
+}
+console.log(myOutfit());
+console.log(outerWear);
+
+var sum = 0;
+function addthree(){
+    sum = sum +3;
+}
+
+function nextInline(arr, item){
+    arr.push(item);
+    return arr.shift();
+}
+var testArr = [1,2,3,4,5];
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInline(testArr , 6));
+console.log("After: " + JSON.stringify(testArr));
+
+
