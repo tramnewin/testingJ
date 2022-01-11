f174264777e200a031d7e00def6b429f53c95d7d
tramnewin <trmnguyen2608@gmail.com>
2022-01-10 17:04:58 -0700

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 console.log("Refresh on Java")
 /* Data types
 undefined, null, boolean, string, symbol, number, and object
+undefined: may have something that you have not set yet
+null: nothing, set sth to nothing
+symbol: immutable primitive val that is unique
+object: can store a lot of diff key value
+of set data to a var, a label to point to a data, easy to mainipulate data type
  */
+//var myName = "Tr";
+//myName = 8;
+//let ourName = "sth";
+const pi = 3.14;
+//var is going to be used throughout the whole prog, let can only be used within the scope you declare that
+var a; //declaring
+var b; //initializing var or so called assignment and declaring at the same time
+a = 7; //assignment
+b = a;
+console.log(b);
+
+
